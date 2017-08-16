bce0a496a0a0be4fdf862a2cec31ec96cf0ca3e7
Joseph Murat <ilovemusiclive@gmail.com>
2017-08-16 17:08:16 +0200

    Test push, I just added index and new file

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,21 @@
 class BooksController < ApplicationController
+
+  def index
+    @books = Book.all.order("created_at DESC")
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :description, :date, :language, :country, :publisher, :meta_title, :meta_description, :permalink, :no_index)
+  end
+
 end
